667fd169ff98bc8de43b0a7471de8348d6f61321
Sam Schick <schicksw@gmail.com>
2016-04-24 13:10:38 -0400

    removed combine and modified regress to focus on text. Added folder for paper.

--- regress.py
+++ regress.py
@@ -1,32 +1,29 @@
-import sys
 import os
 import math
-import numpy
-from combine import CombiningClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
-from datetime import datetime
 import sklearn.cross_validation as cv
 
-folder = sys.argv[1]
-names = os.listdir(folder)
-titles = list()
-texts = list()
-targets = list()
-revCounts = list()
-revRates = list()
-dates = list()
-extraVecs=list()
-for f in names:  # grabs information from files
-    cur = open(f + ".dat").readlines()
-    texts.append(cur[0]+cur[2])
-    targets.append(math.log(eval(cur[1])))
-    revCounts.append(eval(cur[3]))
-    revRates.append(eval(cur[4]))
-    dates.append(datetime.strptime(cur[5], "%D"))
-    extraVecs.append((eval(cur[3]),eval(cur[4]),len(cur[2])))
-extract = TfidfVectorizer(stop_words='english')
-data = extract.fit_transform(texts)
-numpy.insert(data, 0, extraVecs, axis=0)
-learner = CombiningClassifier()
-quality = cv.cross_val_score(learner, data, targets, cv=10, n_jobs=-1)
-print(quality)
+"""Takes a folder with .dat files formatted as specified and extracts their data.
+
+usage(in a python shell for interactivity); from regress.py import main; main(path/to/folder)
+
+This prints the quality assessment of the regressor and leave it saved in the variable 'learner' for further use. """
+
+
+def main(args):
+    folder = args
+    names = os.listdir(folder)
+    texts = list()
+    targets = list()
+    for f in names:  # grabs information from files
+        cur = open(f + ".dat").readlines()
+        texts.append(f+cur[1])
+        targets.append(math.log(eval(cur[0])))
+    extract = TfidfVectorizer(stop_words='english')
+    print("extracting text...")
+    data = extract.fit_transform(texts)
+    learner = RandomForestRegressor()
+    print("evaluating regressor...")
+    quality = cv.cross_val_score(learner, data, targets, cv=10, n_jobs=-1)
+    print(quality)
